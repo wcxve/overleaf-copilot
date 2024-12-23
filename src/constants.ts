c47fb3e91842dec37896c4f760ee8cdc89076ce0
xuewc <wcxuemail@gmail.com>
2024-12-24 06:16:38 +0800

    Set default model to `gpt-4o-mini`

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const LOCAL_STORAGE_KEY_API_KEY = 'config-api-key';
 export const LOCAL_STORAGE_KEY_BASE_URL = 'config-base-url';
 export const LOCAL_STORAGE_KEY_MODEL = 'config-model';
 
-export const DEFAULT_MODEL = 'gpt-3.5-turbo';
+export const DEFAULT_MODEL = 'gpt-4o-mini';
 export const DEFAULT_SUGGESTION_MAX_OUTPUT_TOKEN = 100;
 export const MAX_LENGTH_BEFORE_CURSOR = 5000;
 export const MAX_LENGTH_AFTER_CURSOR = 5000;
